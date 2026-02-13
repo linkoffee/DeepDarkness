e69a2a43e5ae35517d0329446c0f9c7da7169f8f
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2026-02-13 18:14:32 +0500

    Init: Command Parser (parse user input from terminal)

---
+++ Assets/Scripts/Commands/CommandParser.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+public static class CommandParser
+{
+    public static Command Parse(string playerInput)
+    {
+        playerInput = playerInput.Trim().TrimEnd(';');
+
+        var moveRightMatch = Regex.Match(playerInput, @"Knight\.MoveRight\((\d+)\)");
+        if (moveRightMatch.Success)
+            return new MoveCommand("right", int.Parse(moveRightMatch.Groups[1].Value));
+
+        var moveLeftMatch = Regex.Match(playerInput, @"Knight\.MoveLeft\((\d+)\)");
+        if (moveLeftMatch.Success)
+            return new MoveCommand("left", int.Parse(moveLeftMatch.Groups[1].Value));
+
+        var moveUpMatch = Regex.Match(playerInput, @"Knight\.MoveUp\((\d+)\)");
+        if (moveUpMatch.Success)
+            return new MoveCommand("up", int.Parse(moveUpMatch.Groups[1].Value));
+
+        var moveDownMatch = Regex.Match(playerInput, @"Knight\.MoveDown\((\d+)\)");
+        if (moveDownMatch.Success)
+            return new MoveCommand("down", int.Parse(moveDownMatch.Groups[1].Value));
+
+        switch (playerInput)
+        {
+            case "Knight.Attack()":
+                return new AttackCommand();
+            case "Knight.Block()":
+                return new BlockCommand();
+            case "Knight.PickUp()":
+                return new PickUpCommand();
+            default:
+                return null;
+        }
+    }
+}
