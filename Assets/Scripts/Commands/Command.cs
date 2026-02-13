a1643539bb2994cf65a306931fca2020b78c0b64
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2026-02-13 18:13:32 +0500

    Init: Command abstract class

---
+++ Assets/Scripts/Commands/Command.cs
@@ -0,0 +1,45 @@
+﻿public abstract class Command
+{
+    public abstract void Execute(Player player);
+}
+
+public class MoveCommand : Command
+{
+    private readonly string _direction;
+    private readonly int _stepCount;
+
+    public string Direction => _direction;
+    public int StepCount => _stepCount;
+
+    public MoveCommand(string direction, int stepCount)
+    {
+        _direction = direction;
+        _stepCount = stepCount;
+    }
+    
+    public override void Execute(Player player)
+    {
+        switch (_direction.ToLower())
+        {
+            case "up": player.MoveUp(_stepCount); break;
+            case "down": player.MoveDown(_stepCount); break;
+            case "left": player.MoveLeft(_stepCount); break;
+            case "right": player.MoveRight(_stepCount); break;
+        }
+    }
+}
+
+public class AttackCommand : Command
+{
+    public override void Execute(Player player) => player.Attack();
+}
+
+public class BlockCommand : Command
+{
+    public override void Execute(Player player) => player.Block();
+}
+
+public class PickUpCommand : Command
+{
+    public override void Execute(Player player) => player.PickUp();
+}
