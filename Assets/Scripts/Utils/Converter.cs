16198fc329bc65d01e58a95435889e3c5d979992
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2026-03-05 20:03:41 +0500

    add utility: convert arabic to roman number

---
+++ Assets/Scripts/Utils/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+
+public class Converter
+{
+    public static string ToRoman(int number)
+    {
+        if (number < 1 || number > 3999)
+            return "Too Big Number to Convert. Must be in scope (1-3999)";
+
+        int[] numberValues = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        string[] romanSymbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        System.Text.StringBuilder result = new System.Text.StringBuilder();
+
+        for (int i = 0; i < numberValues.Length; i++)
+        {
+            while (number >= numberValues[i])
+            {
+                number -= numberValues[i];
+                result.Append(romanSymbols[i]);
+            }
+        }
+        return result.ToString();
+    }
+}
