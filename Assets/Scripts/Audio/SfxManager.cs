0a13a8335a681ef368876a03a16f84952e7ae0d5
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2026-03-11 20:10:50 +0500

    add sfx manager singletone (play sounds in scenes)

---
+++ Assets/Scripts/Audio/SfxManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour
+{
+    public static SfxManager Instance;
+
+    [SerializeField] private SfxLib sfxLib;
+    [SerializeField] private AudioSource sfxSource;
+
+    public void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void PlaySound3D(AudioClip audioClip, Vector3 position)
+    {
+        if (audioClip != null)
+        {
+            AudioSource.PlayClipAtPoint(audioClip, position);
+        }
+    }
+
+    public void PlaySound3D(string sfxName, Vector3 position)
+    {
+        PlaySound3D(sfxLib.GetClipFromName(sfxName), position);
+    }
+
+    public void PlaySound2D(string sfxName)
+    {
+        sfxSource.PlayOneShot(sfxLib.GetClipFromName(sfxName));
+    }
+}
