c443a10331d3daa621250bcb1f7d9e4566d85770
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2026-03-16 18:37:12 +0500

    Update: Player sprite now flips current direction, attracted to specified object (enemy, pickup, etc.)

--- Assets/Scripts/Player/PlayerVisual.cs
+++ Assets/Scripts/Player/PlayerVisual.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
@@ -22,12 +20,19 @@ public class PlayerVisual : MonoBehaviour
     private const string IsTakingDamageParam = "IsTakingDamage";
     private const string MoveXParam = "MoveX";
     private const string MoveYParam = "MoveY";
-    
+
+    [Header("Target Settings")]
+    [SerializeField] private float interactionRadius = 1.5f;
+    [SerializeField] private List<string> targetTags = new List<string> { "Enemy", "Pickup" };
+
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
 
     private Vector2 _lastPosition;
     private Vector2 _moveDirection;
+    private Vector2 _lastMoveDirection = Vector2.right;
+
+    private Transform _currentTarget;
 
 
     private void Awake()
@@ -51,8 +56,26 @@ public class PlayerVisual : MonoBehaviour
         _moveDirection = (currentPosition - _lastPosition).normalized;
 
         UpdateMovementAnimation();
+        FindNearestTarget();
 
         _lastPosition = currentPosition;
+    }
+
+    private void LateUpdate()
+    {
+        UpdateSpriteFlip();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, interactionRadius);
+
+        if (_currentTarget != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, _currentTarget.position);
+        }
     }
 
     private void OnDestroy()
@@ -80,6 +103,48 @@ public class PlayerVisual : MonoBehaviour
         {
             _animator.SetFloat(MoveX, _moveDirection.x);
             _animator.SetFloat(MoveY, _moveDirection.y);
+
+            if (Mathf.Abs(_moveDirection.x) > 0.01f || Mathf.Abs(_moveDirection.y) > 0.01f)
+                _lastMoveDirection = _moveDirection;
+        }
+    }
+
+    private void FindNearestTarget()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, interactionRadius);
+
+        float nearestDistance = float.MaxValue;
+        Transform nearestTarget = null;
+
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.transform == Player.Instance.transform)
+                continue;
+
+            if (targetTags.Contains(collider.tag))
+            {
+                float distance = Vector2.Distance(transform.position, collider.transform.position);
+                if (distance < nearestDistance)
+                {
+                    nearestDistance = distance;
+                    nearestTarget = collider.transform;
+                }
+            }
+        }
+
+        _currentTarget = nearestTarget;
+    }
+
+    private void UpdateSpriteFlip()
+    {
+        if (_currentTarget != null)
+        {
+            float directionToTarget = _currentTarget.position.x - transform.position.x;
+            _spriteRenderer.flipX = directionToTarget < 0;
+        }
+        else
+        {
+            _spriteRenderer.flipX = _lastMoveDirection.x < 0;
         }
     }
 }
