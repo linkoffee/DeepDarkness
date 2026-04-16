c1bfa3f5bd8131a584dd8b2ed38bb5ffcf7d3725
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2026-04-16 20:13:27 +0500

    update: replace delayed action to special methods
    delete:
    - DelayedAction() method
    - PickUp() method
    add:
    - PerformAttack() method for attack
    - PerformBlock() method for block
    - TakeDamage() method for take damage from enemy
    - CurrentHealth public property to display health status
    - method for on/off player colliders

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,32 +4,66 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] public int maxHealth = 3;
     [SerializeField] private float movingSpeed = 1f;
-    [SerializeField] private int maxHealth = 2;
+
+    [SerializeField] private Collider2D attackCollider;
+    [SerializeField] private Collider2D takeDamageCollider;
+
+    [SerializeField] public int attackDamage = 1;
+    [SerializeField] private float attackCooldown = 2f;
+    [SerializeField] private float blockCooldown = 2f;
 
     public event EventHandler OnPlayerAttack;
     public event EventHandler OnPlayerBlock;
+    public event EventHandler OnPlayerTakeDamage;
+    public event EventHandler OnPlayerDeath;
+
+    public event Action<int> OnHealthChanged;
 
     public static Player Instance { get; private set; }
     public bool IsBusy => _isBusy;
+    public bool IsAttack => _isAttack;
+    public bool IsBlock => _isBlock;
     public bool IsWalking => _isWalking;
+    public bool IsAlive => _isAlive;
+
+    public int CurrentHealth
+    {
+        get => _currentHealth;
+        private set
+        {
+            int newValue = Mathf.Clamp(value, 0, maxHealth);
+
+            if (_currentHealth != newValue)
+            {
+                _currentHealth = newValue;
+                OnHealthChanged?.Invoke(_currentHealth);
+            }
+        }
+    }
+    private int _currentHealth;
+
+    private const float AttackAnimationDuration = 0.3f;
+    private const float BlockAnimationDuration = 0.5f;
 
     private bool _isBusy = false;
     private bool _isAlive;
     private bool _isWalking;
-
-    private int _currentHealth;
-
+    private bool _isAttack;
+    private bool _isBlock;
 
     private void Awake()
     {
         Instance = this;
+        DisableAttackCollider();
+        EnableTakeDamageCollider();
     }
 
     private void Start()
     {
         _isAlive = true;
-        _currentHealth = maxHealth;
+        CurrentHealth = maxHealth;
     }
 
     public void MoveUp(int stepCount)
@@ -59,30 +93,39 @@ public class Player : MonoBehaviour
     public void Attack()
     {
         if (!_isBusy && _isAlive)
-        {
-            OnPlayerAttack?.Invoke(this, EventArgs.Empty);
-            StartCoroutine(DelayedAction());
-        }
+            StartCoroutine(PerformAttack());
     }
 
     public void Block()
     {
         if (!_isBusy && _isAlive)
-        {
-            OnPlayerBlock?.Invoke(this, EventArgs.Empty);
-            StartCoroutine(DelayedAction());
-        }
+            StartCoroutine(PerformBlock());
     }
 
-    public void PickUp()
+    public void TakeDamage(int damage)
     {
-        if (!_isBusy && _isAlive)
-        {
-            Debug.Log("Pick Up");
-            StartCoroutine(DelayedAction());
-        }
+        if (!_isAlive || _isBlock)
+            return;
+
+        CurrentHealth -= damage;
+        OnPlayerTakeDamage?.Invoke(this, EventArgs.Empty);
+
+        if (CurrentHealth <= 0)
+            Die();
+    }
+
+    public void Die()
+    {
+        _isAlive = false;
+        OnPlayerDeath?.Invoke(this, EventArgs.Empty);
     }
     
+    public void EnableAttackCollider() => attackCollider.enabled = true;
+    public void DisableAttackCollider() => attackCollider.enabled = false;
+
+    private void EnableTakeDamageCollider() => takeDamageCollider.enabled = true;
+    private void DisableTakeDamageCollider() => takeDamageCollider.enabled = false;
+
     private IEnumerator Move(Vector3 direction)
     {
         Vector3 targetPosition = transform.position + direction;
@@ -102,10 +145,35 @@ public class Player : MonoBehaviour
         _isBusy = false;
     }
 
-    private IEnumerator DelayedAction(float animationDelay = 1f)
+    private IEnumerator PerformAttack()
     {
         _isBusy = true;
-        yield return new WaitForSeconds(animationDelay);
+        _isAttack = true;
+
+        OnPlayerAttack?.Invoke(this, EventArgs.Empty);
+
+        yield return new WaitForSeconds(AttackAnimationDuration);
+
+        _isAttack = false;
+
+        yield return new WaitForSeconds(attackCooldown);
+        _isBusy = false;
+    }
+
+    private IEnumerator PerformBlock()
+    {
+        _isBusy = true;
+        _isBlock = true;
+
+        DisableTakeDamageCollider();
+        OnPlayerBlock?.Invoke(this, EventArgs.Empty);
+
+        yield return new WaitForSeconds(BlockAnimationDuration);
+
+        _isBlock = false;
+        EnableTakeDamageCollider();
+
+        yield return new WaitForSeconds(blockCooldown);
         _isBusy = false;
     }
 }
