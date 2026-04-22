5b0250bd6fb1ab575a427f9847e80c0404aa6b9f
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2026-04-22 19:03:38 +0500

    update: lock interactable objects on player death

--- Assets/Scripts/UI/Terminal.cs
+++ Assets/Scripts/UI/Terminal.cs
@@ -3,14 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Terminal : MonoBehaviour
 {
     [SerializeField] private TMP_InputField inputField;
     [SerializeField] private TextMeshProUGUI logOutputText;
+    [SerializeField] private Button knowledgeBookButton;
+    [SerializeField] private Button executeButton;
     [SerializeField] private Player player;
-
-    public TMP_InputField InputField => inputField;
 
     private Queue<Command> _commands = new Queue<Command>();
     private Command _currentCommand = null;
@@ -27,11 +28,19 @@ public class Terminal : MonoBehaviour
 
     private void Start()
     {
-        if (InputField != null)
+        if (inputField != null)
         {
-            InputField.Select();
-            InputField.ActivateInputField();
+            inputField.Select();
+            inputField.ActivateInputField();
         }
+
+        if (knowledgeBookButton != null)
+            knowledgeBookButton.interactable = true;
+
+        if (executeButton != null)
+            executeButton.interactable = true;
+
+        player.OnPlayerDeath += OnPlayerDeath;
     }
 
     private void Update()
@@ -41,6 +50,11 @@ public class Terminal : MonoBehaviour
             _currentCommand = null;
             StartCoroutine(ExecuteNext());
         }
+    }
+
+    private void OnDestroy()
+    {
+        player.OnPlayerDeath -= OnPlayerDeath;
     }
 
     public void ExecuteAll()
@@ -80,9 +94,44 @@ public class Terminal : MonoBehaviour
             }
             else
             {
-                AddLogOutput("No valid commands found!", LogOutputType.Warning);
+                AddLogOutput("No valid commands found!", LogOutputType.Error);
             }
         }
+    }
+
+    private void OnPlayerDeath(object sender, EventArgs e)
+    {
+        inputField.interactable = false;
+        knowledgeBookButton.interactable = false;
+        executeButton.interactable = false;
+
+        AddLogOutput("> ==========================");
+        AddLogOutput("> The knight's soul left him");
+        AddLogOutput("> Try to start all over again...");
+    }
+
+    private string GetCommandDescription(Command command)
+    {
+        return command switch
+        {
+            MoveCommand move => $"Knight moves {move.Direction} {move.StepCount} step(s)",
+            AttackCommand => "Attack! The enemy will be crushed!",
+            BlockCommand => "Block! Protect the hull urgently!",
+            _ => command.GetType().Name
+        };
+    }
+
+    private void AddLogOutput(string text, LogOutputType logOutputType = LogOutputType.Info)
+    {
+        Color textColor = logOutputType switch
+        {
+            LogOutputType.Info => new Color(0.65f, 1f, 0.65f),
+            LogOutputType.Warning => Color.yellow,
+            LogOutputType.Error => Color.red,
+            _ => Color.white
+        };
+
+        logOutputText.text += $"<color=#{ColorUtility.ToHtmlStringRGB(textColor)}>{text}</color>\n";
     }
 
     private IEnumerator ExecuteNext()
@@ -99,68 +148,10 @@ public class Terminal : MonoBehaviour
 
         _currentCommand = _commands.Dequeue();
 
-        if (_currentCommand != null )
+        if (_currentCommand != null)
         {
-            AddLogOutput($"> {GetCommandDescription(_currentCommand)}", LogOutputType.Info);
+            AddLogOutput($"> {GetCommandDescription(_currentCommand)}");
             _currentCommand.Execute(player);
         }
-    }
-
-    private string GetCommandDescription(Command command)
-    {
-        return command switch
-        {
-            MoveCommand move => $"Knight moves {move.Direction} {move.StepCount} step(s)",
-            AttackCommand => "Attack! The enemy will be crushed!",
-            BlockCommand => "Block! Protect the hull urgently!",
-            PickUpCommand => "It should be picked up...",
-            _ => command.GetType().Name
-        };
-    }
-
-    //private bool ProcessCommand(string command)
-    //{
-    //    if (string.IsNullOrWhiteSpace(command))
-    //        return false;
-
-    //    return ExecuteCommand(command);
-    //}
-
-    //private bool ExecuteCommand(string commandText)
-    //{
-    //    try
-    //    {
-    //        Command command = CommandParser.Parse(commandText);
-
-    //        if (command != null)
-    //        {
-    //            command.Execute(player);
-    //            AddLogOutput($"> {commandText}");
-    //            return true;
-    //        }
-    //        else
-    //        {
-    //            AddLogOutput($"{commandText} - Unknown command", LogOutputType.Warning);
-    //            return false;
-    //        }
-    //    }
-    //    catch (Exception e)
-    //    {
-    //        AddLogOutput($"{commandText} - Error: {e.Message}", LogOutputType.Error);
-    //        return false;
-    //    }
-    //}
-
-    private void AddLogOutput(string text, LogOutputType logOutputType = LogOutputType.Info)
-    {
-        Color textColor = logOutputType switch
-        {
-            LogOutputType.Info => new Color(0.65f, 1f, 0.65f),
-            LogOutputType.Warning => Color.yellow,
-            LogOutputType.Error => Color.red,
-            _ => Color.white
-        };
-
-        logOutputText.text += $"<color=#{ColorUtility.ToHtmlStringRGB(textColor)}>{text}</color>\n";
     }
 }
