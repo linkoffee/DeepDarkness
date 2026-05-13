19f6fdee3d36d0b921fe7a9d760f282610c5ad83
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2026-05-13 21:41:47 +0500

    add logic to showing picked up item icon
    showing item icon above, while player hold item in the inventory (1 slot)

--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class PlayerInventory : MonoBehaviour
 {
+    [SerializeField] private Sprite _defaultImage;
+    private Image _itemImage;
+
     public static PlayerInventory Instance { get; private set; }
+
     public bool HasKey => _hasKey;
 
     private bool _hasKey;
 
     private void Awake()
     {
+        _itemImage = GetComponent<Image>();
+
         if (Instance == null)
             Instance = this;
         else
@@ -19,14 +27,32 @@ public class PlayerInventory : MonoBehaviour
     {
         Key.OnKeyPickedUp += OnKeyPickedUp;
         Door.OnAnyDoorOpened += OnAnyDoorOpened;
+        PickupEvent.OnItemPickedUp += OnItemPickedUp;
     }
 
     private void OnDisable()
     {
         Key.OnKeyPickedUp -= OnKeyPickedUp;
         Door.OnAnyDoorOpened -= OnAnyDoorOpened;
+        PickupEvent.OnItemPickedUp -= OnItemPickedUp;
     }
 
     private void OnKeyPickedUp() => _hasKey = true;
-    private void OnAnyDoorOpened() => _hasKey = false;
+    private void OnAnyDoorOpened()
+    {
+        _hasKey = false;
+        ResetInventoryView();
+    }
+
+    private void OnItemPickedUp(Sprite itemIcon)
+    {
+        if (_itemImage != null)
+            _itemImage.sprite = itemIcon;
+    }
+
+    private void ResetInventoryView()
+    {
+        if (_itemImage != null)
+            _itemImage.sprite = _defaultImage;
+    }
 }
