4a97265777f984080207a88a2804281e3543f124
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2026-07-01 10:28:01 +0500

    add level content localization settings (keys fields)

--- Assets/Scripts/SceneControllers/LevelController.cs
+++ Assets/Scripts/SceneControllers/LevelController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using DD.Utils;
 using UnityEngine;
 
 public class LevelController : BaseSceneController
@@ -11,18 +12,22 @@ public class LevelController : BaseSceneController
     [SerializeField] public bool enableStorytelling = true;
     [TextArea(10, 20)]
     [SerializeField] public string storytellingText;
+    [SerializeField] public string storytellingTranslationName;
 
     [Header("Notifications")]
     [SerializeField] public bool enableNotifications = false;
     [TextArea(3, 6)]
     [SerializeField] public string notificationText;
+    [SerializeField] public string notificationTranslationName;
     [SerializeField] public float notificationDuration = 3f;
     [SerializeField] public string notificationSound = "DefaultNotification";
 
     [Header("Knowledge Book")]
     [SerializeField] public bool enableKnowledgeUpdate = false;
     [TextArea(10, 20)]
-    [SerializeField] public string contentToAdd;
+    [SerializeField] public string bookContentToAdd;
+    [SerializeField] public string bookContentTranslationName;
+
 
     protected override void PlayMusicTrack()
     {
@@ -32,16 +37,27 @@ public class LevelController : BaseSceneController
 
     protected override void UpdateKnowledgeBookContent()
     {
-        if (enableKnowledgeUpdate && !string.IsNullOrEmpty(contentToAdd))
-        {
-            string id = $"{gameObject.scene.name}_{name}";
-            BookData.Instance.AddContent(contentToAdd, id);
-        }
+        if (!enableKnowledgeUpdate)
+            return;
+
+        string id = $"{gameObject.scene.name}_{name}";
+
+        if (!string.IsNullOrEmpty(bookContentTranslationName))
+            BookData.Instance.AddLocalizedContent(bookContentTranslationName, id);
+        else if (!string.IsNullOrEmpty(bookContentToAdd))
+            BookData.Instance.AddContent(bookContentToAdd, id);
+        else
+            Debug.LogWarning("No content to add: both translation name and fallback text are empty");
     }
 
     protected override IEnumerator RunStorytelling()
     {
-        if (!enableStorytelling || string.IsNullOrEmpty(storytellingText))
+        if (!enableStorytelling)
+            yield break;
+
+        string localizedText = Localizator.GetLocalizedTextWithFallback(storytellingTranslationName, storytellingText);
+
+        if (string.IsNullOrEmpty(localizedText))
             yield break;
 
         bool isClosed = false;
@@ -53,17 +69,22 @@ public class LevelController : BaseSceneController
         }
 
         StorytellerManager.Instance.OnClosed += OnClosed;
-        StorytellerManager.Instance.Show(storytellingText);
+        StorytellerManager.Instance.Show(localizedText);
 
         yield return new WaitUntil(() => isClosed);
     }
 
     protected override IEnumerator ShowNotification()
     {
-        if (!enableNotifications || string.IsNullOrEmpty(notificationText))
+        if (!enableNotifications)
             yield break;
 
-        NotificationManager.Instance.Show(notificationText, notificationDuration, notificationSound);
+        string localizedText = Localizator.GetLocalizedTextWithFallback(notificationTranslationName, notificationText);
+
+        if (string.IsNullOrEmpty(localizedText))
+            yield break;
+
+        NotificationManager.Instance.Show(localizedText, notificationDuration, notificationSound);
 
         yield return new WaitForSeconds(notificationDuration);
     }
